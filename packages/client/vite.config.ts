e4aa5f5986d39bb36b8053e147cfb9dc5e51ce7e
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-07-26 13:57:48 -0400

    fix: Enable HTML minify in prod
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,6 +4,7 @@ import { readdirSync } from "node:fs";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import babelMacrosPlugin from "vite-plugin-babel-macros";
+import htmlMinifier from "vite-plugin-html-minifier";
 import Inspect from "vite-plugin-inspect";
 import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
@@ -28,6 +29,20 @@ export default defineConfig({
       defaultAsComponent: false,
     }),
     addFontPreload(),
+    htmlMinifier({
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
     VitePWA({
       srcDir: "src",
       registerType: "autoUpdate",
